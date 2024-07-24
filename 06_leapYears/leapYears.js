c741451c077921c39c99ffc81486bd8d5040ffc3
wilki52 <williamk3210@gmail.com>
2024-07-24 01:24:31 -0700

    Complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    //for something to be a leapyear, all 2 must be true.
+    //must be divisible by four
+    //must not be divisible by 100 unless they are divisible by 400.
+    if (year % 4 !=0){
+        return false;
+    }
+    if (year%400!=0 && year %100 ==0){
+        return false;
+    }
+
+    return true;
+
+    //must be divisible by 4.
+    //and must be (not divisib)
 
 };
 
