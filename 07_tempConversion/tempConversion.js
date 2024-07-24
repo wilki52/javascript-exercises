6ab3babbd674ad9199be4ec4eea6ea6c7ecee429
wilki52 <williamk3210@gmail.com>
2024-07-24 01:33:07 -0700

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const roundToFirstDecimal = function(number) {
+  return Math.round(number *10)/10;
+
+}
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32)* (5/9);
+  return roundToFirstDecimal(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (9/5)*celsius + 32;
+  return roundToFirstDecimal(fahrenheit);
 };
 
 // Do not edit below this line
