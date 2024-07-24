060e058179405c7d3decfb2accc6c05ade9aaf70
wilki52 <williamk3210@gmail.com>
2024-07-24 01:16:15 -0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if ((typeof int1 != "number") || (int1 % 1 !=0)){
+        return 'ERROR';
+    }
+    if ((typeof int2 != "number") || (int2 % 1 !=0)){
+        return 'ERROR';
+    }
+    if ((int1 <0) || (int2 <0)){
+        return 'ERROR';
+    }
 
+        
+
+    let sum=0;
+    for (let i = Math.min(int1, int2); i <= Math.max(int1, int2); i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
