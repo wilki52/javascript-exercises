10ce752ca8e0a42cfef0ca8f92d467adf908cff4
wilki52 <williamk3210@gmail.com>
2024-07-24 00:22:50 -0700

    Alternate solution to exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,8 @@
 const reverseString = function(string) {
     let reverse_string = "";
 
-    for (let i=string.length-1; i>-1; i--){
-        reverse_string+= string[i];
-    }
-    return reverse_string
+    reverse_string = string.split("").reverse().join("");
+    return reverse_string;
 };
 
 // Do not edit below this line
