f72c7810b8968c582b4a6f948609682cbcf52080
wilki52 <williamk3210@gmail.com>
2024-07-24 00:21:52 -0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse_string = "";
 
+    for (let i=string.length-1; i>-1; i--){
+        reverse_string+= string[i];
+    }
+    return reverse_string
 };
 
 // Do not edit below this line
--- 03_reverseString/solution/reverseString-solution.js
+++ 03_reverseString/solution/reverseString-solution.js
@@ -1,5 +1,7 @@
 const reverseString = function (string) {
   return string.split("").reverse().join("");
+  //split breaks a string using a divider, in this case, "", or blank. so hello becomes [h, e, l, l, o]
+  //reverse this array. then join using the same divider.
 };
 
 module.exports = reverseString;
