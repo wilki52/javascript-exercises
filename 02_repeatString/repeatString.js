00649e5c9163a685b29e4b2c50f7ce3069473410
wilki52 <williamk3210@gmail.com>
2024-07-24 00:16:26 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let word = "";
 
+    if (num < 0){
+        return 'ERROR';
+    }
+    for (let i =0; i< num; i++){
+        word+=string;
+    }
+    return word;
 };
 
 // Do not edit below this line
