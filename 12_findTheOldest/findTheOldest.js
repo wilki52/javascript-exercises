4eb62b0e86cd6b638901bd7755d8ad4e7be93706
wilki52 <williamk3210@gmail.com>
2024-07-27 06:08:54 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    // console.log(people.sort((a, b)=>{
+    //     return ((a.yearOfDeath-a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) ? 1  : -1 ;
+    // }));
+    return people.sort((a, b)=>{
+        if (!('yearOfDeath' in a)){
+            a['yearOfDeath'] = (new Date()).getFullYear();
+
+        }
+        if (!('yearOfDeath' in b)){
+            b['yearOfDeath'] = (new Date()).getFullYear();
+        }
+        return ((a.yearOfDeath-a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) ? 1 : -1;
+    })[0];
+
 
 };
 
