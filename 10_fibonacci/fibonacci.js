5f091def2ccaafa86cd886e386849a7102bfd87f
wilki52 <williamk3210@gmail.com>
2024-07-27 05:45:41 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num==0) return 0;
+    else if (num<0) return 'OOPS';
+    
+
+    fib = [0, 1, 1];
+    for (i=3; i<=num; i++){
+        fib[i] = fib[i-1]+fib[i-2];
+        console.log(fib[i], i, num);
+    }
+    return fib.slice(-1).pop();
 };
 
 // Do not edit below this line
