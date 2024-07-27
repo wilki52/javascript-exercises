1c15b1a9f8539c509ad5b09b72c32cc2ccc0eaf2
wilki52 <williamk3210@gmail.com>
2024-07-27 04:53:21 -0700

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(...nums) {
+  return nums.reduce((sum, item)=> sum+ item, 0);
+};
+
+
+const subtract = function(...nums) {
+  return nums.slice(1).reduce((sum, item)=> sum- item, nums[0]);
+};
+
+const sum = function(arr) {
+	return arr.reduce((sum, item)=>sum+item, 0);
+};
+
+const multiply = function(...nums) {
+  return nums.reduce((product, item)=> product*item, 1);
+};
+
+const power = function(base, exp) {
+  let result = 1;
+  for (let i =0; i<exp; i++){
+    result*=base;
+  }
+
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(integer) {
+  let result = 1;
+	for (let i = integer; i>0; i--){
+    result *=i;
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  }
+  return result;
 };
 
 // Do not edit below this line
